9fa80d2de77a23eced7883a9774d46dbac3e3da8
Sungjo <song9722@gmail.com>
2020-04-27 20:00:35 +0900

    chapter7.section5

---
+++ QuizLocker/app/src/main/java/com/limlab/quizlocker/BootCompleteReceiver.kt
@@ -0,0 +1,18 @@
+package com.limlab.quizlocker
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import android.widget.Toast
+
+class BootCompleteReceiver : BroadcastReceiver(){
+    override fun onReceive(context: Context?, intent: Intent?) {
+        when {
+            intent?.action == Intent.ACTION_BOOT_COMPLETED -> {
+                Log.d("quizlocker", "부팅이 완료됨")
+                Toast.makeText(context, "퀴즈 잠금화면: 부팅이 완료됨", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}

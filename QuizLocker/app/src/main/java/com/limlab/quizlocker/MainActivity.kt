63f35c033a4dd86429eefbbd9c6a807953f55897
Sungjo <song9722@gmail.com>
2020-05-04 14:09:49 +0900

    chapter7.section8

--- QuizLocker/app/src/main/java/com/limlab/quizlocker/MainActivity.kt
+++ QuizLocker/app/src/main/java/com/limlab/quizlocker/MainActivity.kt
@@ -2,11 +2,45 @@ package com.limlab.quizlocker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.MultiSelectListPreference
+import android.preference.PreferenceFragment
 
 class MainActivity : AppCompatActivity() {
+
+    val fragment = MyPreferenceFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // preferenceContent FrameLayout 영역을 PreferenceFragment로 교체
+        fragmentManager.beginTransaction().replace(R.id.preferenceContent, fragment).commit()
+
+    }
+
+    class MyPreferenceFragment : PreferenceFragment() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+
+            // 환경설정 리소스 파일 적용
+            addPreferencesFromResource(R.xml.pref)
+
+            // 퀴즈종류 요약정보에, 현재선택된 항목을 보여주는 코드
+            val categoryPref = findPreference("category") as MultiSelectListPreference
+            categoryPref.summary = categoryPref.values.joinToString(",")
+
+            // 환경설정 정보값이 변경될때에도 요약정보를 변경하도록 리스너 등록
+            categoryPref.setOnPreferenceChangeListener{preference, newValue ->
+                //newValue 파라미터가 HashSet으로 캐스팅이 실패하면 리턴
+                val newValueSet = newValue as? HashSet<*>
+                    ?: return@setOnPreferenceChangeListener true
+
+                //선택된 퀴즈로 요약정보 보여줌
+                categoryPref.summary = newValue.joinToString(",")
+
+                true
+            }
+
+        }
     }
 }
